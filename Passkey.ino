f922f236237636c8f38c469df96ee06bcebd423f
Johannes Findeisen <you@hanez.org>
2023-08-08 02:12:42 +0200

    Moved the passwords array to Passwords.h.

--- Passkey.ino
+++ Passkey.ino
@@ -16,6 +16,9 @@
 // See Blinker.LICENSE for the full license of Blinker.
 #include "Blinker.h"
 
+// You need to edit this file to add your passwords.
+#include "Passwords.h"
+
 // The button pin
 #define BUTTON_PIN 0
 // The LED pin
@@ -29,24 +32,6 @@ unsigned int count;
 
 // Create blinker object
 Blinker blinker(LED_PIN);
-
-// The list of available passwords
-// The number and overall lenght of passwords depends 
-// because the memory of the digispark is very limited...
-// I recommend to use a prefix you easily can remember 
-// for the password/passwords to make the passwords
-// on the passkey unusable for others in case you've lost 
-// your passkey. E.g.: WhatAWonderfulWorld -> then press the button.
-const char *passwords[] = {
-  // passwords[0]
-  "n0Emb871NAQPSIqBoldh8R7UDaNhncF7Pt60Amdo6GWdTdAPwVBi2A3KU8x8DTCRo6GWdTdXNn2wLh3SUbxVWQvFDVPtatDg",
-  // passwords[1]
-  "oaxt6e3lSgflOuJ3C6Q6sUb5gvmvI5IEPFku5fqcbxJljBOUblHIT121wCu",
-  // passwords[2]
-  "jUgKGBtiJ0iNN1Ok9vejrXNn2wLh3SUbxVWQvFDVPtat0OxJlKU8x8DTCRo6GWdTdAPwVBi2A3KUrTKQwjUgK",
-  // passwords[3]
-  "G57dFnXsPUnRq1eC1CjrGxpCjuiJFlDti54W6wBS9Ro6GWdTdAPwVBi2A3KU8x8KGBtiJ0iNN1Ok9vejrXNn2wLhH" 
-};
 
 void setup() {
   // Set button pin to high.
---
+++ Passwords.h
@@ -0,0 +1,17 @@
+// The list of available passwords
+// The number and overall lenght of passwords depends 
+// because the memory of the digispark is very limited...
+// I recommend to use a prefix you easily can remember 
+// for the password/passwords to make the passwords
+// on the passkey unusable for others in case you've lost 
+// your passkey. E.g.: WhatAWonderfulWorld -> then press the button.
+const char *passwords[] = {
+  // passwords[0]
+  "n0Emb871NAQPSIqBoldh8R7UDaNhncF7Pt60Amdo6GWdTdAPwVBi2A3KU8x8DTCRo6GWdTdXNn2wLh3SUbxVWQvFDVPtatDg",
+  // passwords[1]
+  "oaxt6e3lSgflOuJ3C6Q6sUb5gvmvI5IEPFku5fqcbxJljBOUblHIT121wCu",
+  // passwords[2]
+  "jUgKGBtiJ0iNN1Ok9vejrXNn2wLh3SUbxVWQvFDVPtat0OxJlKU8x8DTCRo6GWdTdAPwVBi2A3KUrTKQwjUgK",
+  // passwords[3]
+  "G57dFnXsPUnRq1eC1CjrGxpCjuiJFlDti54W6wBS9Ro6GWdTdAPwVBi2A3KU8x8KGBtiJ0iNN1Ok9vejrXNn2wLhH" 
+};
